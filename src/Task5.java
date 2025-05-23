3f10d7f620cbe2039223d532e9c98a643ab2dbfd
Abildin Avtandil <avtandilabildin2@gmail.com>
2025-05-23 12:38:49 +0600

    homework 2 8 tasks

---
+++ src/Task5.java
@@ -0,0 +1,13 @@
+public class Task5 {
+    public static void main(String[] args) {
+        var frog=3.5;
+        System.out.println(frog);
+        frog*=10;
+        System.out.println(frog);
+        frog/=3.5;
+        System.out.println(frog);
+        frog+=4;
+        System.out.println(frog);
+
+    }
+}
