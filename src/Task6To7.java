9ff417a0138973b1cde8552d7765a3dbb6a9af0a
Abildin Avtandil <avtandilabildin2@gmail.com>
2025-05-27 01:00:56 +0600

    homework 3 8 tasks

--- src/Task6To7.java
+++
@@ -1,9 +0,0 @@
-public class Task6To7 {
-    public static void main(String[] args) {
-        var b1=78.2;
-        var b2=82.7;
-        System.out.println(b1+b2);
-        System.out.println(b1-b2);
-        System.out.println(b2%b1);
-    }
-}
