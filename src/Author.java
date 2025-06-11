8a63d1753a4b66436cea00541ec4ab676463726e
Abildin Avtandil <avtandilabildin2@gmail.com>
2025-06-11 15:08:40 +0600

    methods of objects

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -13,5 +15,27 @@ public class Author {
 
     public String getSurname() {
         return surname;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Author author)) return false;
+
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(surname);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author author;
@@ -5,6 +7,21 @@ public class Book {
 
     public void setDateCreated(int dateCreated) {
         this.dateCreated = dateCreated;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Book book)) return false;
+
+        return dateCreated == book.dateCreated && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(title);
+        result = 31 * result + Objects.hashCode(author);
+        result = 31 * result + dateCreated;
+        return result;
     }
 
     public String getTitle() {
@@ -23,5 +40,14 @@ public class Book {
         this.title = title;
         this.author = author;
         this.dateCreated = dateCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", dateCreated=" + dateCreated +
+                '}';
     }
 }
