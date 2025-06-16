93a59cd9f59dbeba5f0b8942ec5265e610416904
Abildin Avtandil <avtandilabildin2@gmail.com>
2025-06-16 12:27:42 +0600

    course work 1

---
+++ src/Employee.java
@@ -0,0 +1,98 @@
+import java.util.Objects;
+
+public class Employee {
+    private static int id=0;
+    private String name;
+    private String middleName;
+    private String surname;
+    private int department;
+    private int salary;
+
+    public Employee(String name, int department, String surname, String middleName,int salary) {
+        this.name = name;
+        this.department = department;
+        this.surname = surname;
+        this.middleName = middleName;
+        this.salary=salary;
+        id++;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Employee.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Employee employee)) return false;
+
+        return department == employee.department && Objects.equals(name, employee.name) && Objects.equals(middleName, employee.middleName) && Objects.equals(surname, employee.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(middleName);
+        result = 31 * result + Objects.hashCode(surname);
+        result = 31 * result + department;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{"
+                +"id="+id+
+                ", name='" + name + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +'\''+
+                ", department=" + department +
+                '}';
+    }
+    public String withoutDep() {
+        return id + " " + name + " " + middleName + " " + surname+" "+salary;
+    }
+}
--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,51 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        Employee[] employees = new Employee[10];
-        Employee employee1=new Employee("employee1",1,"employee1","employee1",12_000);
-        Employee employee2=new Employee("employee2",2,"employee2","employee2",12_000);
-        Employee employee3=new Employee("employee3",3,"employee3","employee3",12_000);
-        Employee employee4=new Employee("employee4",4,"employee4","employee4",12_000);
-        Employee employee5=new Employee("employee5",5,"employee5","employee5",12_000);
-        Employee employee6=new Employee("employee6",1,"employee6","employee6",12_000);
-        Employee employee7=new Employee("employee7",2,"employee7","employee7",12_000);
-        Employee employee8=new Employee("employee8",3,"employee8","employee8",12_000);
-        Employee employee9=new Employee("employee9",4,"employee9","employee9",12_000);
-        Employee employee10=new Employee("employee10",5,"employee10","employee10",12_000);
-        employees[0]=employee1;
-        employees[1]=employee2;
-        employees[2]=employee3;
-        employees[3]=employee4;
-        employees[4]=employee5;
-        employees[5]=employee6;
-        employees[6]=employee7;
-        employees[7]=employee8;
-        employees[8]=employee9;
-        employees[9]=employee10;
-        EmployeeBook employeeBook = new EmployeeBook();
-        employeeBook.addEmployee(employees[0]);
-        employeeBook.addEmployee(employees[1]);
-        employeeBook.addEmployee(employees[2]);
-        employeeBook.addEmployee(employees[3]);
-        employeeBook.addEmployee(employees[4]);
-        employeeBook.addEmployee(employees[5]);
-        employeeBook.addEmployee(employees[6]);
-        employeeBook.addEmployee(employees[7]);
-        employeeBook.addEmployee(employees[8]);
-        employeeBook.addEmployee(employees[9]);
-    }
-    public static void printAllEmployees(Employee[] employees) {
+import java.util.Arrays;
+
+public class EmployeeBook {
+    private Employee[] employees=new Employee[10];
+    private int counter=0;
+    public void printAllEmployees(Employee[] employees) {
         for (int i = 0; i < employees.length ; i++) {
             System.out.println(employees[i]);
         }
     }
-    public static void sumOfSalary(Employee[] employees) {
+    public void sumOfSalary(Employee[] employees) {
         int sum=0;
         for (int i = 0; i < employees.length; i++) {
             sum+=employees[i].getSalary();
         }
         System.out.println("Сумма зарплат: "+sum);
     }
-    public static void minimumSalary(Employee[] employees) {
+    public void minimumSalary(Employee[] employees) {
         int minimumSalary=employees[0].getSalary();
         for (int i = 1; i < employees.length; i++) {
             if (employees[i].getSalary()<minimumSalary) {
@@ -54,7 +24,7 @@ public class Main {
         }
         System.out.println("Минимальная зарплата: "+minimumSalary);
     }
-    public static void maximumSalary(Employee[] employees) {
+    public void maximumSalary(Employee[] employees) {
         int maximumSalary=employees[0].getSalary();
         for (int i = 1; i < employees.length; i++) {
             if (employees[i].getSalary()>maximumSalary) {
@@ -63,7 +33,7 @@ public class Main {
         }
         System.out.println("Максимальная зарплата: "+maximumSalary);
     }
-    public static void averageSalary(Employee[] employees) {
+    public void averageSalary(Employee[] employees) {
         int sum=0;
         for (int i = 0; i < employees.length; i++) {
             sum+=employees[i].getSalary();
@@ -71,19 +41,19 @@ public class Main {
         int averageSalary=sum/employees.length;
         System.out.println("Среднее значение зарплат: "+averageSalary);
     }
-    public static void nameMiddleNameSurname(Employee[] employees) {
+    public void nameMiddleNameSurname(Employee[] employees) {
         for (int i = 0; i < employees.length; i++) {
             System.out.println(employees[i].getName()+" "+employees[i].getMiddleName()+" "+employees[i].getSurname());
         }
     }
-    public static void percentageSalary(Employee[] employees, int percentage) {
+    public void percentageSalary(Employee[] employees, int percentage) {
         for (int i = 0; i < employees.length; i++) {
             int percent=employees[i].getSalary()*10/100;
             int result=employees[i].getSalary()+percent;
             employees[i].setSalary(result);
         }
     }
-    public static void minSalaryDepartment(Employee[] employees, int department) {
+    public void minSalaryDepartment(Employee[] employees, int department) {
         int minSalary=employees[0].getSalary();
         for (int i = 1; i < employees.length; i++) {
             if(employees[i].getDepartment()==department) {
@@ -94,7 +64,7 @@ public class Main {
         }
         System.out.println("Минимальная зарплата в отделе: "+minSalary);
     }
-    public static void maxSalaryDepartment(Employee[] employees, int department) {
+    public void maxSalaryDepartment(Employee[] employees, int department) {
         int maxSalary=employees[0].getSalary();
         for (int i = 1; i < employees.length; i++) {
             if(employees[i].getDepartment()==department) {
@@ -105,7 +75,7 @@ public class Main {
         }
         System.out.println("Максимальная зарплата по отделу: "+maxSalary);
     }
-    public static void sumSalaryDepartment(Employee[] employees, int department) {
+    public void sumSalaryDepartment(Employee[] employees, int department) {
         int sumSalary=0;
         for (int i = 0; i < employees.length; i++) {
             if(employees[i].getDepartment()==department) {
@@ -114,7 +84,7 @@ public class Main {
         }
         System.out.println("Сумма зарплат: "+sumSalary);
     }
-    public static void averageSalaryDepartment(Employee[] employees, int department) {
+    public void averageSalaryDepartment(Employee[] employees, int department) {
         int count = 0;
         int sumSalary=0;
         for (int i = 0; i < employees.length; i++) {
@@ -126,7 +96,7 @@ public class Main {
         int averageSalary=sumSalary/count;
         System.out.println("Средняя зарплата по отделу: "+averageSalary);
     }
-    public static void percentageSalaryDepartment(Employee[] employees, int department) {
+    public void percentageSalaryDepartment(Employee[] employees, int department) {
         for (int i = 0; i < employees.length; i++) {
             if(employees[i].getDepartment()==department) {
                 int percent=employees[i].getSalary()*10/100;
@@ -135,25 +105,78 @@ public class Main {
             }
         }
     }
-    public static void printEmployeesOfDepartment(Employee[] employees, int department) {
+    public void printEmployeesOfDepartment(Employee[] employees, int department) {
         for (int i = 0; i < employees.length; i++) {
             if(employees[i].getDepartment()==department) {
                 System.out.println(employees[i].withoutDep());
             }
         }
     }
-    public static void lessThan(Employee[] employees,int salary){
+    public void lessThan(Employee[] employees,int salary){
         for (int i = 0; i < employees.length; i++) {
             if(employees[i].getSalary()<salary) {
                 System.out.println(employees[i].withoutDep());
             }
         }
     }
-    public static void moreThan(Employee[] employees,int salary){
+    public void moreThan(Employee[] employees,int salary){
         for (int i = 0; i < employees.length; i++) {
             if(employees[i].getSalary()>=salary) {
                 System.out.println(employees[i].withoutDep());
             }
         }
+    }
+    public boolean addEmployee(Employee employee) {
+        employees[counter]=employee;
+        counter++;
+        if(counter==employees.length) {
+            return false;
+        }
+        return true;
+    }
+    public void removeEmployee(int id) {
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i].getId()==id) {
+                employees[i]=null;
+            }
+        }
+    }
+    public Employee getEmployee(int id) {
+        return employees[id];
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof EmployeeBook that)) return false;
+
+        return Arrays.equals(employees, that.employees);
+    }
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Employee[] employees) {
+        this.employees = employees;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(employees);
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeBook{" +
+                "employees=" + Arrays.toString(employees) +
+                '}';
     }
 }
