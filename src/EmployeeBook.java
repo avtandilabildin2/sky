b1f3372708c25a822ebb672573d674df05f64125
Abildin Avtandil <avtandilabildin2@gmail.com>
2025-06-16 16:39:47 +0600

    course work 1

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -129,7 +129,7 @@ public class EmployeeBook {
     public boolean addEmployee(Employee employee) {
         employees[counter]=employee;
         counter++;
-        if(counter==employees.length-1) {
+        if(counter==employees.length) {
             return false;
         }
         return true;
